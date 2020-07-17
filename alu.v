7a394d0664a081d5c39285a3ecc31c0c64ffa87b
MuzzzammilMia <61595835+MuzzzammilMia@users.noreply.github.com>
2020-07-17 18:35:16 +0100

    Add files via upload
    alu more operations have been added, to increase the functionality of the component.
    alu_tb2 - A more robust test bench with more empathizes on randomized testing.

--- alu.v
+++ alu.v
@@ -5,28 +5,37 @@ Date started: 12/07/2020
 
 module alu(in, sum);
  input [11:0] in;
- wire  [3:0]opcode;
+ wire [3:0]opcode;
  wire [3:0]a;
  wire [3:0]b;
  wire [7:0]c;
  wire [3:0]d;
+ wire [3:0]e;
+ wire [7:0]f;
  output reg [3:0] sum;
  
 
-  assign  opcode = in[11:8];  
-  assign  a = in[7:4];
-  assign  b = in[3:0];
-  assign  c[7:0] = {a,b};
-  assign  d[3:0] = c >> 4;
+ assign  opcode = in[11:8];  
+ assign  a = in[7:4];
+ assign  b = in[3:0];
+ assign  c[7:0] = {a,b};
+ assign  f[7:0] = {a,b};
+ assign  d[3:0] = c >> 1;
+ assign  e[3:0] = f << 1;
 
 always@*
-   case(opcode) 
-    0:  sum = a ^ b;
-    1:  sum = a + b;
-    2:  sum = a - b;
-    3:  sum = d;
+  case(opcode)
+    0:  sum = a || b; // or
+    1:  sum = ~(a || b); //nor
+    2:  sum = a ^ b; //xor
+    3:  sum = a & b;//and
+    4:  sum = ~(a & b); //nand
+    5:  sum = a + b; //addition
+    6:  sum = a - b; //subtraction
+    7:  sum = d; //right shift 4
+    8:  sum = e; //lleft shift 4
     default: sum = 0;
-   endcase
+  endcase
   
 endmodule 
 
--- alu_tb2.v
+++ alu_tb2.v
@@ -7,19 +7,17 @@ Date started: 12/07/2020
 module alu_tb2;
     reg [11:0] in;
     wire [3:0]sum;
+    integer i;
     
    
     localparam period = 20;  
-
     alu UUT (.in(in), .sum(sum));
     
-     initial
-      begin
-        in = $urandom_range(11); 
-        #20 in = $urandom_range(11);
-	#20 in = $urandom_range(11); 
-	#20 in = $urandom_range(11);
-	 
-	#80 $finish;
+    initial begin
+       in = $urandom_range(11);
+       for (i=0; i<10; i=i+1)begin
+        $display("current loop#%0d",i); 
+        #20 in = $urandom_range(0,4095); 
+	end 
       end
 endmodule
